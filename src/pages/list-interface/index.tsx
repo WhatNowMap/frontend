c60967c53844a7e390be6991748579eb210ae972
Thomas Tsoi <tsoithomas@outlook.com>
2023-08-10 20:59:04 -0400

    Apply dark color scheme

--- src/pages/list-interface/index.tsx
+++ src/pages/list-interface/index.tsx
@@ -4,7 +4,15 @@ const ListView = () => {
 
     return (
         <>
-        <EventItem />
+        <EventItem 
+            title="Code Til' You Drop Party"
+            location="164 Eglinton Ave E, Toronto, ON M4P 1A6"
+            category="Entertainment"
+            likes={520}
+            dislikes={134}
+            time={Date.now()}
+            bookmark={true}
+        />
         </>
     )
 

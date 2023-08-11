f380f7e1739ea83cb7dca7ad711dfc93bbf69c0a
Thomas Tsoi <tsoithomas@outlook.com>
2023-08-10 23:24:35 -0400

    Implemented dark mode with props

--- src/components/eventItem.tsx
+++ src/components/eventItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import icon_bookmark_off from "../assets/images/icon-bookmark-off.svg"
 import icon_bookmark_on from "../assets/images/icon-bookmark-on.svg"
 import icon_location_logo from "../assets/images/icon-location-logo.svg"
@@ -8,6 +9,7 @@ import icon_thumb_up from "../assets/images/icon-thumb-up.svg"
 interface EvenItemProps {
     title: string,
     image?: string,
+    link?: string,
     location: string,
     category: string,
     likes: number,
@@ -19,43 +21,68 @@ interface EvenItemProps {
 
 
 const EventItem = (props: EvenItemProps) => {
-    return (
-        <div className="w-full box-border flex flex-row p-4 bg-gray-900">
+    const timediff:number = (Date.now()-props.time)/1000;
+    let timelapse:string = "";
+
+    if (timediff < 2*60) {
+        timelapse = "a moment ago";
+    }
+    else if (timediff < 60*60) {
+        timelapse = Math.floor(timediff/60) + " mins ago"
+    }
+    else {
+        const hr = Math.floor(timediff/60/60);
+        timelapse = hr == 1 ? "1 hr ago" : hr + " hrs ago";
+    }
+
+    const content = (
+        <>
             <div className="w-1/3 h-24 bg-gray-500 me-2">
-                <img src="https://www.torontorentals.com/blog/wp-content/uploads/new-years-eve.jpg" className="w-full h-24 object-cover" />
+                <img src={props.image ? props.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Toronto_Skyline_Summer_2020.jpg/320px-Toronto_Skyline_Summer_2020.jpg"} className="w-full h-24 object-cover" />
             </div>
             <div className="w-2/3 flex flex-col text-gray-900 relative">
                 <div className="absolute top-0 right-0">
                     {
-                    props.bookmark
-                    ? <img src={icon_bookmark_on} /> 
-                    :< img src={icon_bookmark_off} />
+                    props.bookmark 
+                    ? <img src={icon_bookmark_on} />
+                    : <img src={icon_bookmark_off} />
                     }
                 </div>
-                <div className="text-xs text-gray-200">{props.category}</div>
+                <div className="text-xs text-gray-300">{props.category}</div>
                 <div className="font-bold text-gray-100">{props.title}</div>
-                <div className="text-[8pt] text-gray-200">
+                <div className="text-[8pt] text-gray-300">
                     <img className="h-4 mt-1 float-left me-1"  src={icon_location_logo} />
                     {props.location}
                 </div>
                 <div className="w-full h-full flex flex-row grow items-end">
                     <div className="flex flex-row items-center me-4">
                         <img className="h-3 w-3" src={icon_thumb_up} />
-                        <div className="text-xs ms-1 text-gray-200" text-gray-200>{props.likes}</div>
+                        <div className="text-xs ms-1 text-gray-200">{props.likes}</div>
                     </div>
                     <div className="flex flex-row items-center me-4">
                         <img className="h-3 w-3" src={icon_thumb_down} />
                         <div className="text-xs ms-1 text-gray-200">{props.dislikes}</div>
                     </div>
                     <div className="text-end text-sm grow text-gray-200">
-                        {props.time}
+                        {timelapse}
                     </div>
                 </div>
             </div>
-
-
-        </div>
+        </>
     )
+
+    return props.link 
+        ? (
+            <Link to={props.link} className="w-full box-border flex flex-row px-4 py-2 bg-gray-900 hover:bg-gray-800">
+                {content}
+            </Link>
+        )
+        : (
+            <div className="w-full box-border flex flex-row px-4 py-2 bg-gray-900">
+                {content}
+            </div>
+        )
+
 }
  
 export default EventItem;
--- src/pages/list-interface/index.tsx
+++ src/pages/list-interface/index.tsx
@@ -10,7 +10,43 @@ const ListView = () => {
             category="Entertainment"
             likes={520}
             dislikes={134}
-            time={Date.now()}
+            time={Date.now() - Math.floor(Math.random()*10000000)}
+            bookmark={true}
+        />
+        <EventItem 
+            title="Code Til' You Drop Party"
+            location="164 Eglinton Ave E, Toronto, ON M4P 1A6"
+            category="Entertainment"
+            likes={520}
+            dislikes={134}
+            time={Date.now() - Math.floor(Math.random()*10000000)}
+            bookmark={true}
+        />
+        <EventItem 
+            title="Code Til' You Drop Party"
+            location="164 Eglinton Ave E, Toronto, ON M4P 1A6"
+            category="Entertainment"
+            likes={520}
+            dislikes={134}
+            time={Date.now() - Math.floor(Math.random()*10000000)}
+            bookmark={false}
+        />
+        <EventItem 
+            title="Code Til' You Drop Party"
+            location="164 Eglinton Ave E, Toronto, ON M4P 1A6"
+            category="Entertainment"
+            likes={520}
+            dislikes={134}
+            time={Date.now() - Math.floor(Math.random()*10000000)}
+            bookmark={false}
+        />
+        <EventItem 
+            title="Code Til' You Drop Party"
+            location="164 Eglinton Ave E, Toronto, ON M4P 1A6"
+            category="Entertainment"
+            likes={520}
+            dislikes={134}
+            time={Date.now() - Math.floor(Math.random()*10000000)}
             bookmark={true}
         />
         </>

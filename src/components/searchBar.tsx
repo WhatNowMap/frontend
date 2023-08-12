8e1f0c18b6a253e13798a3a491e26c466b7fafad
Thomas Tsoi <tsoithomas@outlook.com>
2023-08-12 00:08:42 -0400

    Added categories in search bar

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,4 +1,5 @@
 import logo from "../assets/images/logo-circle.svg";
+import * as defaults from "../utils/constants";
 
 interface SearchBarProps {
     sort?: boolean
@@ -9,16 +10,33 @@ const SearchBar = (props: SearchBarProps) => {
 
 
     return (
-        <div className="w-full bg-primary-700 z-50 p-4">
-            <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
-                <img src={logo} className="w-7 h-7" />
+        <div className="z-50">
+            <div className="w-full bg-primary-700 p-4">
+                <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+                    <img src={logo} className="w-7 h-7" />
+                </div>
+                <input type="text" id="search" className="bg-white border border-secondary-300 text-secondary-800 text-md rounded-3xl focus:ring-primary-300 focus:border-primary-300 block w-full pl-14 p-2.5" placeholder="Search" />
+                </div>
             </div>
-            <input type="text" id="search" className="bg-white border border-secondary-300 text-secondary-800 text-md rounded-3xl focus:ring-primary-300 focus:border-primary-300 block w-full pl-14 p-2.5" placeholder="Search" />
+
+            <div className="w-full p-2 bg-secondary-700">
+                <div className="flex flex-nowrap w-full overflow-x-scroll">
+                {
+                    defaults.Categories.map((category) => {
+                        return <span className="inline-block mx-1 py-1 px-3 text-xs rounded-xl bg-primary-100 text-primary-700" key={category}>{category}</span>
+                    })
+
+                }
+                </div>
             </div>
 
 
         </div>
+
+
+
+
     )
 }
  

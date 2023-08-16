f55d92aab91ca2c2276d9f66121fcf45cbb45da0
Miu Ha <wmh0226@gmail.com>
2023-08-16 16:00:59 -0400

    get API for userName and patch for update userName

--- src/components/user-profile-and-setting/userProfileAndSetting.tsx
+++ src/components/user-profile-and-setting/userProfileAndSetting.tsx
@@ -1,26 +1,65 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const UserProfileAndSetting: React.FC = () => {
-  const [name, setName] = useState<string>("Name"); //would be fetch get from backend
-  const [inputValue, setInputValue] = useState<string>("Name"); //get input value
+  const [userName, setUserName] = useState<string>(""); //get input value
   const [showInput, setShowInput] = useState<boolean>(false); //edit name
 
   const getNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-
-  const changeNameHandler = () => {
-    //would be post to backend
-    setName(inputValue);
-    setShowInput(!showInput);
+    setUserName(e.target.value);
   };
 
   const cancleHandler = () => {
-    setShowInput(!showInput);
+    setShowInput(false);
   };
 
   const editHandler = () => {
-    setShowInput(!showInput);
+    setShowInput(true);
+  };
+
+  const fetchUserNameHandler = async (): Promise<void> => {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/user/profile/64d52a54e06e42dc494b7436"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      console.log(data.userName);
+      setUserName(data.userName);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect((): void => {
+    fetchUserNameHandler();
+  }, [fetchUserNameHandler]);
+
+  const updateUserName = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/user/update/64d52a54e06e42dc494b7436",
+        {
+          method: "PATCH",
+          body: JSON.stringify({ userName: userName }), // Use the variable without quotes
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      setShowInput(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const content = (
@@ -36,11 +75,11 @@ const UserProfileAndSetting: React.FC = () => {
               type="text"
               onChange={getNameHandler}
               className="bg-secondary-800 px-2 py-1 w-full focus:outline-none"
-              defaultValue={name}
+              defaultValue={userName}
             />
           </div>
         ) : (
-          <div className=" w-3/4 pl-2 pb-2 border-b ml-3">{name}</div>
+          <div className=" w-3/4 pl-2 pb-2 border-b ml-3">{userName}</div>
         )}
       </div>
 
@@ -58,7 +97,7 @@ const UserProfileAndSetting: React.FC = () => {
             </button>
             <button
               className=" ml-2 bg-primary-600  py-1 px-3 rounded-md"
-              onClick={changeNameHandler}
+              onClick={updateUserName}
             >
               Change
             </button>

cf3be5d5858a30f7086343fe68bc9bc9d3779e13
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-08-16 14:01:01 -0400

    feat: google login intergration

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,27 @@ import {
 } from 'react-router-dom';
 
 import LandingPage from './pages/landing-page';
-import UserRegisterOrLogin from './pages/register-login-page';
 import RootLayout from './pages/root-layout';
 import ListView from './pages/list-interface';
 import EventDetail from './pages/event-detail';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<LandingPage />} />
-      <Route path="/register" element={<UserRegisterOrLogin />} />
-      <Route path="/list" element={<ListView />} />
-      <Route path="/event" element={<EventDetail />} />
+    <Route
+      path='/'
+      element={<RootLayout />}>
+      <Route
+        index
+        element={<LandingPage />}
+      />
+      <Route
+        path='/list'
+        element={<ListView />}
+      />
+      <Route
+        path='/event'
+        element={<EventDetail />}
+      />
     </Route>
   )
 )

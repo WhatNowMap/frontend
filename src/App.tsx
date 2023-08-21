2152902bf2dd2f441eaee72ca5644e420c4e95ba
Thomas Tsoi <tsoithomas@outlook.com>
2023-08-21 13:10:20 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import EventDetail from "./pages/event-detail";
 import MapView from "./pages/map-interface";
 import UserProfile from "./pages/user-profile";
 import NotFound from "./pages/not-found";
+import Bookmark from "./pages/bookmark";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,10 +29,7 @@ const router = createBrowserRouter(
       <Route path="/event/:event_id" element={<EventDetail />} />
       <Route path="/profile" element={<UserProfile />} />
       <Route path="*" element={<NotFound />} />
-<<<<<<< HEAD
       <Route path="/bookmark" element={<Bookmark />} />
-=======
->>>>>>> 2791934040a8733624f5fb5c4a485e4393f31509
     </Route>
   )
 );
